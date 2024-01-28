c270d363dda268887f190b2a7a057d736ad02a90
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-01-28 04:14:12 +0200

    add lab4

--- LAB3/Lab2.1/src/app/login/login.component.ts
+++ LAB3/Lab2.1/src/app/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterLink,RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,RouterLink,RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  LoginForm = {
+    email: "",
+    password: "",
+  }
+  handleFormSubmit(form : any){
+    console.log(form)
+    console.log(this.LoginForm)
+  }
 }
--- LAB3/Lab2.1/src/app/register/register.component.ts
+++ LAB3/Lab2.1/src/app/register/register.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterLink,RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule,RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
+      name: ['', Validators.required],
+      username: ['', [Validators.required, Validators.pattern(/^\S*$/)]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
+      ]]
+    });
+  }
+
+  get email() { return this.registerForm.get('email'); }
+  get name() { return this.registerForm.get('name'); }
+  get username() { return this.registerForm.get('username'); }
+  get password() { return this.registerForm.get('password'); }
+
+  handleFormSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Form is valid');
+      console.log(this.registerForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
